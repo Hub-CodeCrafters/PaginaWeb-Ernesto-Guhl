6bb24b6beed17d718bd62aa1b30aba1a45c218f0
Your Name <you@example.com>
2023-12-28 10:30:51 -0500

    "buid(dataEnMemoria.js):Se renderizaron las citas y se agregaron fotos de los autores"

--- assets/data/libros_Ernesto/dataEnMemoria.js
+++ assets/data/libros_Ernesto/dataEnMemoria.js
@@ -14,89 +14,76 @@ const dataEnMemoria = [
     {
         name:'El tiempo',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita1.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita1.jpg" 
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita1.jpg",
+        url_img:"../assets/images/imagenes_en_memoria/Logo-Tiempo.jpg"   
     },
 
     {
         name:'El tiempo',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita2 (1).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita2.jpg"  
+        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita2.pdf",
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita2.jpg",
+        url_img:"../assets/images/imagenes_en_memoria/Logo-Tiempo.jpg"  
     },
-    
-
-    {
-        name:'El tiempo',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita2 (2).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita2.jpg"  
-    },
-    
+      
     {
         name:'Santiago Diaz Benavides',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita3 (1).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita3.jpg",
+        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita3.pdf",
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita3.jpg" ,
         url_img:"../assets/images/imagenes_en_memoria/Santiago-Diaz-Benavides.jpg" 
     },
     
-    {
-        name:'Santiago Diaz Benavides',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita3 (2).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita3.jpg"  
-    },
     
-    {
-        name:'Santiago Diaz Benavides',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita3 (3).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita2.jpg"  
-    },
-    
-    {
-        name:'Santiago Diaz Benavides',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita3 (4).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita3.jpg"  
-    },
     
     {
         name:'El espectador ',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita4.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita4.jpg"  
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita4.jpg",
+        url_img:"../assets/images/imagenes_en_memoria/Espectador-logo.png" 
+        
     },
 
     {
         name:'El tiempo ',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita5.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita5.jpg"  
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita5.jpg"  ,
+        url_img:"../assets/images/imagenes_en_memoria/Logo-Tiempo.jpg" 
     },
     
     {
         name:'El tiempo',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita6.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita6.jpg"  
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita6.jpg" ,
+        url_img:"../assets/images/imagenes_en_memoria/Logo-Tiempo.jpg" 
     },
 
     {
         name:'El tiempo',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita7.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita7.jpg"  
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita7.jpg"  ,
+        url_img:"../assets/images/imagenes_en_memoria/Logo-Tiempo.jpg" 
     },
 
     {
         name:'El tiempo',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita8.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita8.jpg"  
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita8.jpg",
+        url_img:"../assets/images/imagenes_en_memoria/Logo-Tiempo.jpg"   
     },
 
     {
         name:'El tiempo',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita9.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita9.jpg"  
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita9.jpg" ,
+        url_img:"../assets/images/imagenes_en_memoria/Logo-Tiempo.jpg" 
     },
 
     {
         name:'El tiempo',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita10.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita10.jpg"  
-    },
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita10.jpg"  ,
+        url_img:"../assets/images/imagenes_en_memoria/Logo-Tiempo.jpg" 
 
+   },
     {
         name:'Julia Mariana Gulligen Becerra',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita11.pdf",
@@ -115,50 +102,28 @@ const dataEnMemoria = [
     {
         name:'Fundación Juan Pablo Gutiérrez Cáceres',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita14.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita14.jpg"  
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita14.jpg",
+        url_img:"../assets/images/imagenes_en_memoria/fundacion-juan-pablo.jpg"  
     },
 
     {
         name:'Alfredo Lewin y Maria Consuelo de Lewin ',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita15 (1).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita15.jpg"  
-    },
-
-    {
-        name:'Alfredo Lewin y Maria Consuelo de Lewin ',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita15 (2).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita15.jpg"  
-    },
-
-    {
-        name:' Victor Alberto Gomez cusnir',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita15 (3).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita15.jpg", 
-        url_img:"../assets/images/imagenes_en_memoria/Victor-Alberto-Gomez-cusnir.jpg", 
-    },
-
-    {
-        name:' Victor Alberto Gomez cusnir',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita15 (4).pdf",
+        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita15.pdf",
         url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita15.jpg"  
     },
 
     {
         name:'Pan de vida CER',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita16 (1).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita16.jpg"  
-    },
-
-    {
-        name:'Pan de vida CER',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita16 (2).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita16.jpg"  
+        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita16.pdf",
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita16.jpg"  ,
+        url_img:"../assets/images/imagenes_en_memoria/pan-de-vida-cer.png" 
     },
 
     {
         name:' Banco de Bogota',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita17.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita17.jpg"  
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita17.jpg"  ,
+        url_img:"../assets/images/imagenes_en_memoria/banco-de-bogota.jpg"
     },
 
     {
@@ -168,29 +133,27 @@ const dataEnMemoria = [
     },
 
     {
-        name:'Paola Andrea Barrera Sanchez',
+        name:'',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita19.pdf",
         url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita19.jpg",  
-        url_img:"../assets/images/imagenes_en_memoria/ Paola-Andrea-Barrera-Sanchez.jpg" 
+        url_img:"../assets/images/imagenes_en_memoria/ideal-fundacion.jpg" 
+        
     },
        
     {
-        name:' ',
+        name:' Paola Andrea Barrera Sanchez',
         url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita20.pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita20.jpg"  
+        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita20.jpg"  ,
+        url_img:"../assets/images/imagenes_en_memoria/Paola-Andrea-Barrera-Sanchez.jpg" 
     },
 
     {
         name:' ',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita21 (1).pdf",
+        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita21.pdf",
         url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita21.jpg"  
     },
 
-    {
-        name:' ',
-        url_palabras:"../assets/Pdf_libros_Ernesto/pdf_en_memoria/cita21 (2).pdf",
-        url_img:"../assets/images/libros_Ernesto/img_en_memoria/cita21.jpg"  
-    },
+
 
     
     // duplicados para ejemplos de cuando hallan mas 
